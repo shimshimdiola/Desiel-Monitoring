e7fcf32081031b9896f759c57a72df534e5eb3fd
Kian Shim Diola <shimshimdiola@gmail.com>
2025-10-06 23:28:19 +0800

    First commit 10.6.25-11:28pm

--- config/config.php
+++ config/config.php
@@ -4,10 +4,11 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 // Site configuration: make sure the name matches your folder name
-define("SITE_NAME", "PHP-Boilerplate");
+define("SITE_NAME", "Desiel");
 // Database name without prefix/suffix
-define("DB_NAME", "Boilerplate");
+define("DB_NAME", "Desiel");
 // Base URL of the application
 $host = $_SERVER['HTTP_HOST'];
 // Build base URL dynamically
 define("BASE_URL", "http://" . $host . "/" . SITE_NAME . "/");
+//  echo BASE_URL;
